a23c5f834da4be6393dfd0fd88cd15e8b43070a6
haythem-said <haythem.said97@gmail.com>
2024-02-22 21:46:42 +0100

    add client script

--- app.js
+++ app.js
@@ -5,22 +5,27 @@ import { Server } from "socket.io";
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
-});
+const PORT = 3000;
+
+const users = {};
+
 io.on("connection", (socket) => {
-  console.log("Un client est connecté");
-
-  socket.on("message", (data) => {
-    console.log("Message reçu du client:", data);
-    io.emit("message", data); // renvoie le message à tous les clients connectés
+  socket.on("new-user", (name) => {
+    users[socket.id] = name;
+    socket.broadcast.emit("user-connected", name);
   });
-
+  socket.on("send-chat-message", (message) => {
+    socket.broadcast.emit("chat-message", {
+      message: message,
+      name: users[socket.id],
+    });
+  });
   socket.on("disconnect", () => {
-    console.log("Un client s'est déconnecté");
+    socket.broadcast.emit("user-disconnected", users[socket.id]);
+    delete users[socket.id];
   });
 });
 
-server.listen(3000, () => {
-  console.log("Serveur Socket.IO démarré sur le port 3000");
+server.listen(PORT, () => {
+  console.log(`L'application écoute sur le port ${PORT}!`);
 });
